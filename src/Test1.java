69a800b763f41ccd68e6ccb3d103ab02f2d3d0d9
XY59005 <XY59005@WSAMZN-HLBJ87PA.za.omlac.net>
2021-12-28 09:12:25 +0200

    version 2

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,11 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Hello world ! ");
+        System.out.println("Hello world ! ");  
+        
+        for (int i=0;i<-1;i++) {
+        	System.out.print("Hi1 ");
+        }
 	}
-
+	
 }
